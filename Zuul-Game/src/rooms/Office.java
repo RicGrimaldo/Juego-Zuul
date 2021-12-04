8caec33c34268b2f8276c02111eb766262ada6fd
AndreiTorres <andreitorres09@outlook.com>
2021-12-04 14:31:47 -0600

    Singleton Office

---
+++ Zuul-Game/src/rooms/Office.java
@@ -0,0 +1,17 @@
+public class Office extends Room {
+    private static Office office;
+
+    private Office(String description) {
+        super(description);
+        //TODO Auto-generated constructor stub
+    }
+
+    public static Office getInstance(String description) {
+        if (office == null) {
+            office = new Office(description);
+        }
+
+        return office;
+    }
+    
+}
