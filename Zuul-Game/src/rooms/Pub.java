582dd8415ed20ba710ddd83fdc5b24e4a84f210e
AndreiTorres <andreitorres09@outlook.com>
2021-12-04 14:33:12 -0600

    Singleton Pub

---
+++ Zuul-Game/src/rooms/Pub.java
@@ -0,0 +1,17 @@
+public class Pub extends Room{
+    private static Pub pub;
+
+    private Pub(String description) {
+        super(description);
+        //TODO Auto-generated constructor stub
+    }
+
+    public static Pub getInstance(String description) {
+        if (pub == null) {
+            pub = new Pub(description);
+        }
+
+        return pub;
+    }
+    
+}
