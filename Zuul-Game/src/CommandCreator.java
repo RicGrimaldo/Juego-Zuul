ee0d0fa8598e745b42db418af15b358583e7d3bc
Ana Perez <perezyamana@outlook.com>
2021-12-04 18:22:20 -0600

    Lectura de las palabras que conforman el comando

--- Zuul-Game/src/CommandCreator.java
+++ Zuul-Game/src/CommandCreator.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+
 public class CommandCreator {
-    
+    private Scanner scCommandWords;
+
+    public Command createCommand(String input){
+        String commandWord = null;
+        String secondWord = null;
+        scCommandWords = new Scanner(input);
+
+        if(scCommandWords.hasNext()) {
+            commandWord = scCommandWords.next();
+            if(scCommandWords.hasNext()) {
+                secondWord = scCommandWords.next();
+            }
+        }
+        return new Command(commandWord, secondWord);
+    }
 }
